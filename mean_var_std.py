3a690591b83ac095bd1fce5773c75489a5a9711d
jessislearning a <jessica.afalla.29@gmail.com>
2024-03-24 04:51:24 +0000

    receive list, create matrix, calculate mean

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,12 @@
 import numpy as np
 
 def calculate(list):
+    nparray = np.array(list)
+    matrix = nparray.reshape((3,3))
+
+    mean0 = matrix.mean(0)
+    mean1 = matrix.mean(1)
 
 
-
-
-    return calculations
+    return mean1
+    #calculations
